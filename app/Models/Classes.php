611b18e4b7426e03a83a4dd7b7803584a411d694
wannnnnn <syazwanbunander1997@gmail.com>
2025-06-09 16:59:45 +0800

    migratecolumntoClass&adjustModelforclassbasedONcolumn

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -10,12 +10,10 @@ class Classes extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'email',
-        'class',
-        'age',
-        'marks',
-        'grade',
-        'year',
-    ];
+            'calss','year','bil', 'tingkatan','subject','bilangan_calon', 'calon_ambil', 'th', 'a_plus_bil', 'a_plus_percent',
+            'a_bil', 'a_percent', 'a_minus_bil', 'a_minus_percent', 'b_plus_bil',
+            'b_plus_percent', 'b_bil', 'b_percent', 'c_plus_bil', 'c_plus_percent',
+            'c_bil', 'c_percent', 'd_bil', 'd_percent', 'e_bil', 'e_percent',
+            'g_bil', 'g_percent', 'gp', 'lulus_percent'
+        ];
 }
